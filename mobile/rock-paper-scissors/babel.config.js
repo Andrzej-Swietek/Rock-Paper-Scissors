9fc0e6da22c4d23d5149661ee798334d3df20440
Andrzej Swietek <mr.andi321@gmail.com>
2023-10-27 19:14:54 +0200

    Added babel config and updated packed json with all required libs

--- mobile/rock-paper-scissors/babel.config.js
+++ mobile/rock-paper-scissors/babel.config.js
@@ -1,6 +1,28 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+      presets: ['babel-preset-expo'],
+      plugins: [
+          [
+              "module-resolver",
+              {
+                  alias: {
+                      "shared": "./src/shared",
+                      "screens": "./src/screens",
+                      "navigation": "./src/navigation",
+                      "components": "./src/components",
+                      "hooks": "./src/shared/hooks"
+                  },
+              },
+          ],
+          ["module:react-native-dotenv", {
+              "moduleName": "@env",
+              "path": ".env",
+              "blacklist": null,
+              "whitelist": null,
+              "safe": false,
+              "allowUndefined": true
+          }]
+      ]
   };
 };
