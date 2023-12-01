9259bdad128e3a75145e3bce521f39074bba72ef
Andrzej Świętek <mr.andi321@gmail.com>
2023-12-01 23:10:12 +0100

    Added Primary Button style

--- frontend/src/types/button.d.ts
+++ frontend/src/types/button.d.ts
@@ -1,10 +1,13 @@
 interface ButtonProps {
     className?: string;
-    onClick: ()=> void;
+    onClick?: ()=> void;
     href?: string;
+    variant: ButtonVariant
 }
 
 interface IconButtonProps extends ButtonProps  {
     icon?: string;
     iconSize?: string;
 }
+
+type ButtonVariant = "filled" | "outlined" | "text" | "disabled"
